263aac58f7ae6b210bf1eaef6a3f735aa8c56ed1
aleks <aleks@DESKTOP-K6OCHKE>
2023-12-01 07:16:41 +0500

    Добавьте файлы проекта.

---
+++ DataModels/Helper.cs
@@ -0,0 +1,14 @@
+﻿namespace tyuiu.oop.ZhukovDA.IsakovAV.StudentHelper.DataModels
+{
+    public class Helper
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public DateTime Duration { get; set; }
+
+        public void SetDateWithDaysAhead(int daysAhead)
+        {
+            Duration = DateTime.Now.AddDays(daysAhead);
+        }
+    }
+}
